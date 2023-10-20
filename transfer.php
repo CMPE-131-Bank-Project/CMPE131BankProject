6e1c36e34b871f22ab87266f3a8ed64ff815a0b5
KennethFam <147673498+KennethFam@users.noreply.github.com>
2023-10-19 18:49:07 -0700

    Add files via upload

---
+++ transfer.php
@@ -0,0 +1,35 @@
+<?php
+    session_start();
+    if(isset($_SESSION['logged_in']) == FALSE) header("Location: Login.html");
+    else {
+        if (isset($_POST['account']) && isset($_POST['recipient']) && isset($_POST['amount'])) {
+            if ($_POST['account'] && $_POST['recipient'] && $_POST['amount']) {
+                $conn = mysqli_connect("localhost", "root", "", "users");
+                if (!$conn) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
+                $account = $_POST['account'];
+                $recipient = $_POST['recipient'];
+                $amount = $_POST['amount'];
+                $sql = "SELECT * FROM BankAccounts WHERE accountNum = '$account'";
+                $result = mysqli_query($conn, $sql);
+                $bal = mysqli_fetch_assoc($result);
+                $bal['Balance'] = $bal['Balance'] - $amount;
+                $nbal = $bal['Balance'];
+                $sql = "UPDATE BankAccounts SET Balance = '$nbal' WHERE accountNum = '$account'";
+                $result = mysqli_query($conn, $sql);
+                $sql = "SELECT * FROM BankAccounts WHERE accountNum = '$recipient'";
+                $result = mysqli_query($conn, $sql);
+                $bal = mysqli_fetch_assoc($result);
+                $bal['Balance'] = $bal['Balance'] + $amount;
+                $nbal = $bal['Balance'];
+                $sql = "UPDATE BankAccounts SET Balance = '$nbal' WHERE accountNum = '$recipient'";
+                $result = mysqli_query($conn, $sql);
+                mysqli_close($conn);
+                header("Location: user.php");
+            }
+            else header("Location: user.php");
+        }
+        else header("Location: user.php");
+    } 
+?>
