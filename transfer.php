5a9f30bc775a16669a1e7f65752e83c0c420395f
KennethFam <147673498+KennethFam@users.noreply.github.com>
2023-10-21 13:20:38 -0700

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -16,17 +16,23 @@
                 $bal = mysqli_fetch_assoc($result);
                 $bal['Balance'] = $bal['Balance'] - $amount;
                 $nbal = $bal['Balance'];
-                $sql = "UPDATE BankAccounts SET Balance = '$nbal' WHERE accountNum = '$account'";
-                $result = mysqli_query($conn, $sql);
-                $sql = "SELECT * FROM BankAccounts WHERE accountNum = '$recipient'";
-                $result = mysqli_query($conn, $sql);
-                $bal = mysqli_fetch_assoc($result);
-                $bal['Balance'] = $bal['Balance'] + $amount;
-                $nbal = $bal['Balance'];
-                $sql = "UPDATE BankAccounts SET Balance = '$nbal' WHERE accountNum = '$recipient'";
-                $result = mysqli_query($conn, $sql);
-                mysqli_close($conn);
-                header("Location: user.php");
+                if ($nbal < 0) {
+                    mysqli_close($conn);
+                    echo "<script>alert('Overdraft: Inadequate Balance in Sender Account to Perform Transfer');window.location.href='user.php';</script>";
+                } 
+                else {
+                    $sql = "UPDATE BankAccounts SET Balance = '$nbal' WHERE accountNum = '$account'";
+                    $result = mysqli_query($conn, $sql);
+                    $sql = "SELECT * FROM BankAccounts WHERE accountNum = '$recipient'";
+                    $result = mysqli_query($conn, $sql);
+                    $bal = mysqli_fetch_assoc($result);
+                    $bal['Balance'] = $bal['Balance'] + $amount;
+                    $nbal = $bal['Balance'];
+                    $sql = "UPDATE BankAccounts SET Balance = '$nbal' WHERE accountNum = '$recipient'";
+                    $result = mysqli_query($conn, $sql);
+                    mysqli_close($conn);
+                    header("Location: user.php");
+                }
             }
             else header("Location: user.php");
         }
