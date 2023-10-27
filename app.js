373e92c8fd5274286bb30da1a119701da0894aae
angie11-11 <147684602+angie11-11@users.noreply.github.com>
2023-10-26 18:24:42 -0700

    Update app.js

--- app.js
+++ app.js
@@ -11,15 +11,3 @@ $(document).ready(function() {
     });
 });
 
-// In your app.js file
-$(document).ready(function() {
-    $('#rememberMe').change(function() {
-        if (this.checked) {
-            // User wants to be remembered, implement your logic here
-            // For example, set a cookie or store the preference in localStorage
-        } else {
-            // User does not want to be remembered, implement your logic here
-            // For example, remove the cookie or clear the preference from localStorage
-        }
-    });
-});
