de9dc437684c01c0fa7f4de2a2cbeefcb8866de3
KennethFam <147673498+KennethFam@users.noreply.github.com>
2023-11-01 20:21:08 -0700

    Update depositprocess.php

--- depositprocess.php
+++ depositprocess.php
@@ -1,5 +1,9 @@
 <?php
-    if (isset($_FILES['frontCheck']) && isset($_FILES['backCheck'])) {
+    session_start();
+    if(isset($_SESSION['logged_in']) == FALSE) header("Location: Login.php");
+    else if ($_SESSION['TFA'] == TRUE && $_SESSION['logged_in'] == FALSE) header("Location: MultiFactor.php");
+    else if ($_SESSION['TFA'] == FALSE && $_SESSION['logged_in'] == FALSE) header("Location: Login.php");
+    else if (isset($_FILES['frontCheck']) && isset($_FILES['backCheck'])) {
         $phpFileUploadErrors = array(
             0 => 'There is no error, the file uploaded with success',
             1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
@@ -26,7 +30,7 @@
             if ($_FILES['frontCheck']['error'] == '4') {
                 echo "<div class='alert alert-danger'>
                 <strong>Error!</strong> At least one file was not uploaded.
-              </div>";
+            </div>";
             }
             else {
             echo $phpFileUploadErrors[$_FILES['frontCheck']['error']];
@@ -36,7 +40,7 @@
             if ($_FILES['backCheck']['error'] == '4') {
                 echo "<div class='alert alert-danger'>
                 <strong>Error!</strong> At least one file was not uploaded.
-              </div>";
+            </div>";
             }
             else {
             echo $phpFileUploadErrors[$_FILES['backCheck']['error']];
@@ -45,50 +49,45 @@
         elseif ($ext_error) {
             echo "<div class='alert alert-danger'>
             <strong>Error!</strong> At least one file is an unsupported type. Please upload files of the following type only: (.jepg .jpg .png .gif)
-          </div>";
+        </div>";
         }
         else {
             move_uploaded_file($_FILES['frontCheck']['tmp_name'], 'images/'.$_FILES['frontCheck']['name']);
             move_uploaded_file($_FILES['backCheck']['tmp_name'], 'images/'.$_FILES['backCheck']['name']);
         }
+        if (!$ext_error) {
+            $logged_in = $_SESSION['logged_in'];
+            $username = $_SESSION['username'];
+            $conn = mysqli_connect("localhost", "root", "", "users");
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+            if (isset($_POST['submit_button'])) {
+                $logged_in = $_SESSION['logged_in'];
+                $username = $_SESSION['username'];
+                $conn = mysqli_connect("localhost", "root", "", "users");
+                if (!$conn) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
+                $accountNum = $_POST['account'];
+                $amount = $_POST['amount'];
+                $frontcheck = 'images/'.$_FILES['frontCheck']['name'];
+                $backcheck = 'images/'.$_FILES['backCheck']['name'];
+                $sql = "INSERT INTO deposits (accountNum, username, amount, frontcheck, backcheck) VALUES ('$accountNum', '$username', '$amount', '$frontcheck', '$backcheck')";
+                $results = mysqli_query($conn, $sql);
+                if ($results) {
+                    echo "<div class='alert alert-success'>
+                    <strong>Success!</strong> Your deposit is being processed.
+                </div>";
+                }
+                else {
+                    echo mysqli_error($conn);
+                }
+            } 
+        }
     }
-    ?>
-<?php
-session_start();
-if(isset($_SESSION['logged_in']) == FALSE) header("Location: Login.html");
-else {
-    if (!$ext_error) {
-    $logged_in = $_SESSION['logged_in'];
-    $username = $_SESSION['username'];
-    $conn = mysqli_connect("localhost", "root", "", "users");
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    if (isset($_POST['submit_button'])) {
-        $logged_in = $_SESSION['logged_in'];
-        $username = $_SESSION['username'];
-        $conn = mysqli_connect("localhost", "root", "", "users");
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-        $accountNum = $_POST['account'];
-        $amount = $_POST['amount'];
-        $frontcheck = 'images/'.$_FILES['frontCheck']['name'];
-        $backcheck = 'images/'.$_FILES['backCheck']['name'];
-        $sql = "INSERT INTO deposits (accountNum, username, amount, frontcheck, backcheck) VALUES ('$accountNum', '$username', '$amount', '$frontcheck', '$backcheck')";
-        $results = mysqli_query($conn, $sql);
-        if ($results) {
-            echo "<div class='alert alert-success'>
-            <strong>Success!</strong> Your deposit is being processed.
-          </div>";
-        }
-        else {
-            echo mysqli_error($conn);
-        }
-    } 
-}
-}
-        ?>
+    else header("Location: user.php")
+?>
 <!DOCTYPE html>
 <html lang="en">  
 <head>
