01c2261b4cfd441abde2fb5183b0d3c9011b6182
KennethFam <147673498+KennethFam@users.noreply.github.com>
2023-11-03 12:51:50 -0700

    Update deposit.php

--- deposit.php
+++ deposit.php
@@ -76,8 +76,8 @@
             mysqli_close($conn);
         ?> 
 
-    <div class="popup" id ="popup-3">
-                <div class="overlay"></div>
+        <div class="popup" id ="popup-3">
+            <div class="overlay"></div>
                 <div class="content">
                     <p class="timertext" style="font-size: 1.5rem;"> 
                         <span class="secs"></span>
@@ -90,26 +90,13 @@
                             var popUpInactive = true;
 
                             $(document).ready(function() { 
-
-                                /* Increment the idle time 
-                                    counter every second */ 
                                 let idleInterval = 
                                     setInterval(timerIncrement, 1000); 
-
-                                /* Zero the idle timer 
-                                    on mouse movement */ 
                                     document.addEventListener("wheel", function (e) {
-                                        // get the old value of the translation (there has to be an easier way than this)
                                         var oldVal = parseInt(document.getElementById("body").style.transform.replace("translateY(","").replace("px)",""));
-
-                                        // to make it work on IE or Chrome
                                         var variation = parseInt(e.deltaY);
-
-                                        // update the body translation to simulate a scroll
                                         document.getElementById("body").style.transform = "translateY(" + (oldVal - variation) + "px)";
-
                                         return false;
-
                                     }, true);
                                     $(this).on("mousemove keypress keyup keypress keydown touchstart click dblclick scroll wheel load unload", function() {
                                         if(popUpInactive) resetTimer();
@@ -117,11 +104,8 @@
                             });
 
                             function resetTimer() { 
-
-                                /* Hide the timer text */ 
                                 document.querySelector(".timertext") 
                                     .style.display = 'none'; 
-
                                 currSeconds = 0; 
                             } 
 
@@ -141,9 +125,6 @@
                                 dSeconds = left % 60;
                                 Minutes = Math.trunc(left/60); 
                                 if (dSeconds < 10) dSeconds = "0" + dSeconds;
-
-                                /* Set the timer text to 
-                                    the new value */ 
                                 if (currSeconds == 300) {
                                     popUpInactive = false;
                                     document.getElementById("popup-3").classList.toggle("active");
@@ -151,8 +132,6 @@
                                 if (currSeconds >= 300 && left >= 0) {
                                     document.querySelector(".secs") 
                                     .textContent = "Due to inactivity, you will be logged out in " + Minutes + ":" + dSeconds; 
-
-                                    /* Display the timer text */ 
                                     document.querySelector(".timertext") 
                                         .style.display = 'block'; 
                                 }
@@ -161,8 +140,7 @@
                     </script>
                 </div>
             </div>
-
-    </div>
+        </div>
     </div>
     </body>
 </html>
