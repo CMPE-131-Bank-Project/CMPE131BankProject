ded0ddcaf24054e5517d285e914bb78b4fafa810
KennethFam <147673498+KennethFam@users.noreply.github.com>
2023-11-03 12:48:42 -0700

    Update depositprocess.php

--- depositprocess.php
+++ depositprocess.php
@@ -75,6 +75,11 @@
                 $backcheck = 'images/'.$_FILES['backCheck']['name'];
                 $sql = "INSERT INTO deposits (accountNum, username, amount, frontcheck, backcheck) VALUES ('$accountNum', '$username', '$amount', '$frontcheck', '$backcheck')";
                 $results = mysqli_query($conn, $sql);
+                date_default_timezone_set('America/Los_Angeles');
+                $date = date("m/d/Y");
+                $time = date("h:i:sa");
+                $sql = "INSERT INTO Transactions (accountNum, date_occured, time_occured, transaction_type, transaction_status, amount, location) VALUES ('$accountNum', '$date', '$time', 'Deposit', 'Processing', '$amount', '$accountNum')";
+                $result = mysqli_query($conn, $sql);
                 if ($results) {
                     echo "<div class='alert alert-success'>
                     <strong>Success!</strong> Your deposit is being processed.
@@ -95,8 +100,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>depositprocess</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="depositprocess.css" />
 </head>
 <body>
     <br>
@@ -106,5 +112,70 @@
     <form action="user.php" style="display: inline-block;">
         <button type="submit">Back to Homepage</button>
     </form>
+    <div class="popup" id ="popup-3">
+        <div class="overlay"></div>
+        <div class="content">
+            <p class="timertext" style="font-size: 1.5rem;"> 
+                <span class="secs"></span>
+                <br>
+                <button onclick="stay()">Stay Logged In</button>
+                <button onclick="logout()">Logout</button>
+            </p> 
+                <script type="text/javascript"> 
+                    var currSeconds = 0; 
+                    var popUpInactive = true;
+
+                    $(document).ready(function() { 
+                        let idleInterval = 
+                            setInterval(timerIncrement, 1000); 
+                            document.addEventListener("wheel", function (e) {
+                                var oldVal = parseInt(document.getElementById("body").style.transform.replace("translateY(","").replace("px)",""));
+                                var variation = parseInt(e.deltaY);
+                                document.getElementById("body").style.transform = "translateY(" + (oldVal - variation) + "px)";
+                                return false;
+                            }, true);
+                            $(this).on("mousemove keypress keyup keypress keydown touchstart click dblclick scroll wheel load unload", function() {
+                                if(popUpInactive) resetTimer();
+                            })
+                    });
+
+                    function resetTimer() { 
+                        document.querySelector(".timertext") 
+                            .style.display = 'none'; 
+
+                        currSeconds = 0; 
+                    } 
+
+                    function logout() {
+                        window.location.href='Logout.php';
+                    }
+
+                    function stay() {
+                        popUpInactive = true;
+                        resetTimer();
+                        document.getElementById("popup-3").classList.toggle("active");
+                    }
+
+                    function timerIncrement() { 
+                        currSeconds = currSeconds + 1;
+                        left = 600 - currSeconds;
+                        dSeconds = left % 60;
+                        Minutes = Math.trunc(left/60); 
+                        if (dSeconds < 10) dSeconds = "0" + dSeconds;
+                        if (currSeconds == 300) {
+                            popUpInactive = false;
+                            document.getElementById("popup-3").classList.toggle("active");
+                        }
+                        if (currSeconds >= 300 && left >= 0) {
+                            document.querySelector(".secs") 
+                            .textContent = "Due to inactivity, you will be logged out in " + Minutes + ":" + dSeconds; 
+                            document.querySelector(".timertext") 
+                                .style.display = 'block'; 
+                        }
+                        if (currSeconds == 600) logout();
+                    } 
+            </script>
+        </div>
+    </div>
 </body>
 </html>
