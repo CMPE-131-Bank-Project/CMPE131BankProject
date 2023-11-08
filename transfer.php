175015ee5c25246cebcc3677e1b94adac984f6b0
KennethFam <147673498+KennethFam@users.noreply.github.com>
2023-11-08 09:25:21 -0800

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -11,50 +11,93 @@
             }
             $account = $_POST['account'];
             $recipient = $_POST['recipient'];
-            $amount = $_POST['amount'];
-            $sql = "SELECT Balance FROM BankAccounts WHERE accountNum = '$account'";
+            $sql="SELECT * FROM BankAccounts WHERE AccountNum='$recipient'";
             $result = mysqli_query($conn, $sql);
-            $bal = mysqli_fetch_assoc($result);
-            $bal['Balance'] = $bal['Balance'] - $amount;
-            $nbal = $bal['Balance'];
-            $sql = "SELECT username FROM BankAccounts WHERE accountNum = '$recipient'";
-            $result = mysqli_query($conn, $sql);
-            $recipientUser = mysqli_fetch_assoc($result);
-            $otherUser = $recipientUser['username'];
-            if ($nbal < 0) {
-                mysqli_close($conn);
-                echo "<script>alert('Overdraft: Inadequate Balance in Sender Account to Perform Transfer');window.location.href='user.php';</script>";
-            } 
-            else if ($account == $recipient) {
-                mysqli_close($conn);
-                echo "<script>alert('You cannot transfer money to and from the same account.');window.location.href='user.php';</script>";
-            } 
-            else if ($username == $otherUser) {
-                $sql = "UPDATE BankAccounts SET Balance = '$nbal' WHERE accountNum = '$account'";
-                $result = mysqli_query($conn, $sql);
-                $sql = "SELECT Balance FROM BankAccounts WHERE accountNum = '$recipient'";
+            $existence = mysqli_num_rows($result);
+            if ($existence == 0) echo "<script>alert('Recipient account does not exist.');window.location.href='user.php';</script>";
+            else {
+                $amount = $_POST['amount'];
+                $sql = "SELECT Balance FROM BankAccounts WHERE accountNum = '$account'";
                 $result = mysqli_query($conn, $sql);
                 $bal = mysqli_fetch_assoc($result);
-                $bal['Balance'] = $bal['Balance'] + $amount;
+                $old_sender = $bal['Balance'];
+                $bal['Balance'] = $bal['Balance'] - $amount;
                 $nbal = $bal['Balance'];
-                $sql = "UPDATE BankAccounts SET Balance = '$nbal' WHERE accountNum = '$recipient'";
+                $sql = "SELECT username FROM BankAccounts WHERE accountNum = '$recipient'";
                 $result = mysqli_query($conn, $sql);
-                $date = date("m/d/Y");
-                $time = date("h:i:sa");
-                $sql = "INSERT INTO Transactions (accountNum, date_occured, time_occured, transaction_type, transaction_status, amount, location) VALUES ('$account', '$date', '$time', 'Transfer', 'Processed', '-$amount', '$recipient')";
-                $result = mysqli_query($conn, $sql);
-                $sql = "INSERT INTO Transactions (accountNum, date_occured, time_occured, transaction_type, transaction_status, amount, location) VALUES ('$recipient', '$date', '$time', 'Transferee', 'Processed', '$amount', '$account')";
-                $result = mysqli_query($conn, $sql);
-                mysqli_close($conn);
-                header("Location: user.php");
-            }
-            else {
-                $_SESSION['account'] = $account;
-                $_SESSION['recipient'] = $recipient;
-                $_SESSION['nbal'] = $nbal;
-                $_SESSION['amount'] = $amount;
-                $_SESSION['TFA'] = TRUE;
-                header("Location: GenerateCode.php");
+                $recipientUser = mysqli_fetch_assoc($result);
+                $otherUser = $recipientUser['username'];
+                if ($nbal < 0) {
+                    mysqli_close($conn);
+                    echo "<script>alert('Overdraft: Inadequate Balance in Sender Account to Perform Transfer');window.location.href='user.php';</script>";
+                } 
+                else if ($account == $recipient) {
+                    mysqli_close($conn);
+                    echo "<script>alert('You cannot transfer money to and from the same account.');window.location.href='user.php';</script>";
+                } 
+                else if ($username == $otherUser) {
+                    $sql = "UPDATE BankAccounts SET Balance = '$nbal' WHERE accountNum = '$account'";
+                    $result = mysqli_query($conn, $sql);
+                    $sql = "SELECT Balance FROM BankAccounts WHERE accountNum = '$recipient'";
+                    $result = mysqli_query($conn, $sql);
+                    $bal = mysqli_fetch_assoc($result);
+                    $old_recipient = $bal['Balance'];
+                    $bal['Balance'] = $bal['Balance'] + $amount;
+                    $nbal = $bal['Balance'];
+                    $sql = "UPDATE BankAccounts SET Balance = '$nbal' WHERE accountNum = '$recipient'";
+                    $result = mysqli_query($conn, $sql);
+                    date_default_timezone_set('America/Los_Angeles');
+                    $date = date("m/d/Y");
+                    $time = date("h:i:sa");
+                    $year = (date("Y") - 1001) * pow(10, 8);
+                    $num = $year + rand(0, 99999999);
+                    $sql="SELECT * FROM Transactions WHERE transaction_num='$num'";
+                    $result = mysqli_query($conn, $sql);
+                    $duplicate = mysqli_num_rows($result);
+                    $count = 0;
+                    while ($duplicate > 0) {
+                        if ($count == 1000) {
+                            $year = $year + 1;
+                            $count = 0;
+                        }
+                        $num = $year + rand(0, 99999999);
+                        $sql="SELECT * FROM Transactions WHERE transaction_num='$num'";
+                        $result = mysqli_query($conn, $sql);
+                        $duplicate = mysqli_num_rows($result);
+                        $count = $count + 1;
+                    }
+                    $sql = "INSERT INTO Transactions (transaction_num, accountNum, date_occured, time_occured, transaction_type, transaction_status, amount, location, old_balance) VALUES ('$num', '$account', '$date', '$time', 'Transfer', 'Processed', '-$amount', '$recipient', '$old_sender')";
+                    $result = mysqli_query($conn, $sql);
+                    $num = $year + rand(0, 99999999);
+                    $sql="SELECT * FROM Transactions WHERE transaction_num='$num'";
+                    $result = mysqli_query($conn, $sql);
+                    $duplicate = mysqli_num_rows($result);
+                    $count = 0;
+                    while ($duplicate > 0) {
+                        if ($count == 1000) {
+                            $year = $year + 1;
+                            $count = 0;
+                        }
+                        $num = $year + rand(0, 99999999);
+                        $sql="SELECT * FROM Transactions WHERE transaction_num='$num'";
+                        $result = mysqli_query($conn, $sql);
+                        $duplicate = mysqli_num_rows($result);
+                        $count = $count + 1;
+                    }
+                    $sql = "INSERT INTO Transactions (transaction_num, accountNum, date_occured, time_occured, transaction_type, transaction_status, amount, location, old_balance) VALUES ('$num', '$recipient', '$date', '$time', 'Transferee', 'Processed', '$amount', '$account', '$old_recipient')";
+                    $result = mysqli_query($conn, $sql);
+                    mysqli_close($conn);
+                    header("Location: user.php");
+                }
+                else {
+                    $_SESSION['account'] = $account;
+                    $_SESSION['recipient'] = $recipient;
+                    $_SESSION['nbal'] = $nbal;
+                    $_SESSION['amount'] = $amount;
+                    $_SESSION['old_sender'] = $old_sender;
+                    $_SESSION['TFA'] = TRUE;
+                    header("Location: GenerateCode.php");
+                }
             }
         }
         else header("Location: user.php");
@@ -64,6 +107,7 @@
         $recipient = $_SESSION['recipient'];
         $nbal = $_SESSION['nbal'];
         $amount = $_SESSION['amount'];
+        $old_sender = $_SESSION['old_sender'];
         $_SESSION['TFA'] = FALSE;
         $_SESSION['TFA_Token'] = FALSE;
         $conn = mysqli_connect("localhost", "root", "", "users");
@@ -75,6 +119,7 @@
         $sql = "SELECT Balance FROM BankAccounts WHERE accountNum = '$recipient'";
         $result = mysqli_query($conn, $sql);
         $bal = mysqli_fetch_assoc($result);
+        $old_recipient = $bal['Balance'];
         $bal['Balance'] = $bal['Balance'] + $amount;
         $nbal = $bal['Balance'];
         $sql = "UPDATE BankAccounts SET Balance = '$nbal' WHERE accountNum = '$recipient'";
@@ -82,9 +127,42 @@
         date_default_timezone_set('America/Los_Angeles');
         $date = date("m/d/Y");
         $time = date("h:i:sa");
-        $sql = "INSERT INTO Transactions (accountNum, date_occured, time_occured, transaction_type, transaction_status, amount, location) VALUES ('$account', '$date', '$time', 'Transfer', 'Processed', '-$amount', '$recipient')";
+        $year = (date("Y") - 1001) * pow(10, 8);
+        $num = $year + rand(0, 99999999);
+        $sql="SELECT * FROM Transactions WHERE transaction_num='$num'";
         $result = mysqli_query($conn, $sql);
-        $sql = "INSERT INTO Transactions (accountNum, date_occured, time_occured, transaction_type, transaction_status, amount, location) VALUES ('$recipient', '$date', '$time', 'Transferee', 'Processed', '$amount', '$account')";
+        $duplicate = mysqli_num_rows($result);
+        $count = 0;
+        while ($duplicate > 0) {
+            if ($count == 1000) {
+                $year = $year + 1;
+                $count = 0;
+            }
+            $num = $year + rand(0, 99999999);
+            $sql="SELECT * FROM Transactions WHERE transaction_num='$num'";
+            $result = mysqli_query($conn, $sql);
+            $duplicate = mysqli_num_rows($result);
+            $count = $count + 1;
+        }
+        $sql = "INSERT INTO Transactions (transaction_num, accountNum, date_occured, time_occured, transaction_type, transaction_status, amount, location, old_balance) VALUES ('$num', '$account', '$date', '$time', 'Transfer', 'Processed', '-$amount', '$recipient', '$old_sender')";
+        $result = mysqli_query($conn, $sql);
+        $num = $year + rand(0, 99999999);
+        $sql="SELECT * FROM Transactions WHERE transaction_num='$num'";
+        $result = mysqli_query($conn, $sql);
+        $duplicate = mysqli_num_rows($result);
+        $count = 0;
+        while ($duplicate > 0) {
+            if ($count == 1000) {
+                $year = $year + 1;
+                $count = 0;
+            }
+            $num = $year + rand(0, 99999999);
+            $sql="SELECT * FROM Transactions WHERE transaction_num='$num'";
+            $result = mysqli_query($conn, $sql);
+            $duplicate = mysqli_num_rows($result);
+            $count = $count + 1;
+        }
+        $sql = "INSERT INTO Transactions (transaction_num, accountNum, date_occured, time_occured, transaction_type, transaction_status, amount, location, old_balance) VALUES ('$num', '$recipient', '$date', '$time', 'Transferee', 'Processed', '$amount', '$account', '$old_recipient')";
         $result = mysqli_query($conn, $sql);
         mysqli_close($conn);
         header("Location: user.php");
