151f33456f954add51b8fbeff1781c6fddf0929d
KennethFam <147673498+KennethFam@users.noreply.github.com>
2023-11-08 09:23:19 -0800

    Update Logout.php

--- Logout.php
+++ Logout.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    unset($_SESSION['logged_in'], $_SESSION['TFA'], $_SESSION['username'], $SESSION['body'], $_SESSION['Last_Location'], $_SESSION['account'], $_SESSION['recipient'], $_SESSION['nbal'], $_SESSION['amount']);
+    unset($_SESSION['logged_in'], $_SESSION['TFA'], $_SESSION['username'], $SESSION['body'], $_SESSION['Last_Location'], $_SESSION['account'], $_SESSION['recipient'], $_SESSION['nbal'], $_SESSION['amount'], $_SESSION['old_sender'];);
     session_destroy();
     header("Location: Login.php");
     exit;
