67611f51c624ab9716a400296b82d9723111587b
KennethFam <147673498+KennethFam@users.noreply.github.com>
2023-11-16 13:13:22 -0800

    Add files via upload

---
+++ deposit_final_process.php
@@ -0,0 +1,37 @@
+<?php 
+    if (isset($_POST["account_num"]) && isset($_POST["decision"]) && isset($_POST["transaction_num"]) && isset($_POST["amount"])) {
+        $conn = mysqli_connect("localhost", "root", "", "users");
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        $account = $_POST["account_num"];
+        $decision = $_POST["decision"];
+        $transaction = $_POST["transaction_num"];
+        date_default_timezone_set('America/Los_Angeles');
+        $date = date("m/d/Y");
+        $time = date("h:i:sa");
+        $mtime = date("H:i:s");
+        if ($decision == "accept") {
+            $amount = $_POST["amount"];
+            $sql = "SELECT Balance FROM BankAccounts WHERE accountNum='$account'";
+            $result = mysqli_query($conn, $sql);
+            $info = mysqli_fetch_assoc($result);
+            $obal = $info["Balance"];
+            $bal = $obal + $amount;
+            $sql = "UPDATE Transactions SET date_occured='$date', time_occured='$time', transaction_status='Processed', old_balance='$obal', mil_time='$mtime' WHERE transaction_num='$transaction'";
+            $result = mysqli_query($conn, $sql);
+            $sql = "UPDATE BankAccounts SET Balance='$bal' WHERE accountNum='$account'";
+            $result = mysqli_query($conn, $sql);
+            $sql = "DELETE FROM deposits WHERE transaction_num='$transaction'";
+            $result = mysqli_query($conn, $sql);
+        }
+        else if ($decision == "deny") {
+            $sql = "UPDATE Transactions SET date_occured='$date', time_occured='$time', transaction_status='Denied', mil_time='$mtime' WHERE transaction_num='$transaction'";
+            $result = mysqli_query($conn, $sql);
+            $sql = "DELETE FROM deposits WHERE transaction_num='$transaction'";
+            $result = mysqli_query($conn, $sql);
+        }
+        header("Location: employee.php");
+    }
+    else header("Location: employee.php");
+?>
