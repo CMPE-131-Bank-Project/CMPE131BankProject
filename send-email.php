9b63e91777b433eddad7325b2802ac308f4f92b7
KennethFam <147673498+KennethFam@users.noreply.github.com>
2023-11-19 00:21:29 -0800

    Update send-email.php

--- send-email.php
+++ send-email.php
@@ -4,12 +4,26 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\SMTP;
     if (isset($_SESSION['body']) && $_SESSION['TFA'] == true) {
-        $username = $_SESSION['username'];
         $conn = mysqli_connect("localhost", "root", "", "users");
         if (!$conn) {
             die("Connection failed: " . mysqli_connect_error());
         }
-        $sql = "SELECT email FROM registrations WHERE username = '$username'";
+        if ($_SESSION['Last_Location'] == "authentication.php") {
+            $username = $_SESSION['username'];
+            $sql = "SELECT email FROM registrations WHERE username = '$username'";
+        }
+        else if ($_SESSION['Last_Location'] == "employee_authentication.php") {
+            $id = $_SESSION['id'];
+            $sql = "SELECT email FROM Employees WHERE employee_id = '$id'";
+        }
+        else if ($_SESSION['logged_in'] == TRUE) {
+            $username = $_SESSION['username'];
+            $sql = "SELECT email FROM registrations WHERE username = '$username'";
+        }
+        else if ($_SESSION['e_logged_in'] == TRUE) {
+            $id = $_SESSION['id'];
+            $sql = "SELECT email FROM Employees WHERE employee_id = '$id'";
+        }
         $result = mysqli_query($conn, $sql);
         $info = mysqli_fetch_assoc($result);
         $email = $info['email'];
