a509ea9d8d5657d0955c2e84652e88f93f8b3cd8
KennethFam <147673498+KennethFam@users.noreply.github.com>
2023-11-19 00:04:06 -0800

    Update Logout.php

--- Logout.php
+++ Logout.php
@@ -1,6 +1,10 @@
 <?php
     session_start();
-    unset($_SESSION['logged_in'], $_SESSION['TFA'], $_SESSION['username'], $SESSION['body'], $_SESSION['Last_Location'], $_SESSION['account'], $_SESSION['recipient'], $_SESSION['nbal'], $_SESSION['amount'], $_SESSION['old_sender'];);
+    if ($_SESSION["e_logged_in"] == TRUE) $location = "EmployeeLogin.php";
+    else if ($_SESSION["logged_in"] == TRUE) $location = "Login.php";
+    else $location = "home.php";
+    unset($_SESSION['logged_in'], $_SESSION['TFA'], $_SESSION['username'], $SESSION['body'], $_SESSION['Last_Location'], $_SESSION['account'], $_SESSION['recipient'], $_SESSION['nbal'], $_SESSION['amount'], $_SESSION['old_sender'], $_SESSION['e_logged_in'], $_SESSION['new_e_token'], $_SESSION['id'], $_SESSION['time']);
     session_destroy();
-    header("Location: Login.php");
+    header("Location: $location");
     exit;
+?>
