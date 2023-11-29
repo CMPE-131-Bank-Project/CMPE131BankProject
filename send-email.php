221e35218269d0362a0ac9a4e0a052b785805eab
KennethFam <147673498+KennethFam@users.noreply.github.com>
2023-11-29 10:18:18 -0800

    Update send-email.php

--- send-email.php
+++ send-email.php
@@ -3,30 +3,9 @@
     require "vendor/autoload.php";
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\SMTP;
-    if (isset($_SESSION['body']) && $_SESSION['TFA'] == true) {
-        $conn = mysqli_connect("localhost", "root", "", "users");
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-        if ($_SESSION['Last_Location'] == "authentication.php") {
-            $username = $_SESSION['username'];
-            $sql = "SELECT email FROM registrations WHERE username = '$username'";
-        }
-        else if ($_SESSION['Last_Location'] == "employee_authentication.php") {
-            $id = $_SESSION['id'];
-            $sql = "SELECT email FROM Employees WHERE employee_id = '$id'";
-        }
-        else if ($_SESSION['logged_in'] == TRUE) {
-            $username = $_SESSION['username'];
-            $sql = "SELECT email FROM registrations WHERE username = '$username'";
-        }
-        else if ($_SESSION['e_logged_in'] == TRUE) {
-            $id = $_SESSION['id'];
-            $sql = "SELECT email FROM Employees WHERE employee_id = '$id'";
-        }
-        $result = mysqli_query($conn, $sql);
-        $info = mysqli_fetch_assoc($result);
-        $email = $info['email'];
+    if (isset($_SESSION['body'])) {
+        if (isset($_POST["email"])) $email = $_POST["email"];
+        else $email = $_SESSION['email'];
         $body = $_SESSION['body'];
         $name = "Bank of the Future";
     
@@ -50,6 +29,8 @@
         $mail->Body = $body;
     
         $mail->send();
+
+        unset($_SESSION['body']);
     
         header("Location: MultiFactor.php");
     }
