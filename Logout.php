9e3b40e89d40246f7d0640686561a80468c09dbd
KennethFam <147673498+KennethFam@users.noreply.github.com>
2023-11-29 10:18:49 -0800

    Update Logout.php

--- Logout.php
+++ Logout.php
@@ -3,7 +3,7 @@
     if ($_SESSION["e_logged_in"] == TRUE) $location = "EmployeeLogin.php";
     else if ($_SESSION["logged_in"] == TRUE) $location = "Login.php";
     else $location = "HomePage.html";
-    unset($_SESSION['logged_in'], $_SESSION['TFA'], $_SESSION['username'], $SESSION['body'], $_SESSION['Last_Location'], $_SESSION['account'], $_SESSION['recipient'], $_SESSION['nbal'], $_SESSION['amount'], $_SESSION['old_sender'], $_SESSION['e_logged_in'], $_SESSION['new_e_token'], $_SESSION['id'], $_SESSION['time'], $_SESSION['tier']);
+    unset($_SESSION['logged_in'], $_SESSION['TFA'], $_SESSION['username'], $_SESSION['Last_Location'], $_SESSION['account'], $_SESSION['recipient'], $_SESSION['nbal'], $_SESSION['amount'], $_SESSION['old_sender'], $_SESSION['e_logged_in'], $_SESSION['new_e_token'], $_SESSION['id'], $_SESSION['time'], $_SESSION['tier'], $_SESSION['email']);
     session_destroy();
     header("Location: $location");
     exit;
