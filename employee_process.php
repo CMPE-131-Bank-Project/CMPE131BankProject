07373d096c12694af7f247b4784cfec807f33fd3
KennethFam <147673498+KennethFam@users.noreply.github.com>
2023-11-30 02:43:39 -0800

    Update employee_process.php

--- employee_process.php
+++ employee_process.php
@@ -1,10 +1,9 @@
 <?php 
     session_start();
-    if (isset($_POST["fname"]) && isset($_POST["lname"]) && isset($_POST["password"]) && isset($_POST["email"]) && isset($_POST["ssn"]) && isset($_POST["phone"]) && isset($_POST["lstate"]) && isset($_POST["license"]) && isset($_POST["address"]) && isset($_POST["city"]) && isset($_POST["state"]) && isset($_POST["zip"]) && isset($_SESSION["id"])) {
+    if (isset($_POST["fname"]) && isset($_POST["lname"]) && isset($_POST["password"]) && isset($_POST["ssn"]) && isset($_POST["phone"]) && isset($_POST["lstate"]) && isset($_POST["license"]) && isset($_POST["address"]) && isset($_POST["city"]) && isset($_POST["state"]) && isset($_POST["zip"]) && isset($_SESSION["id"])) {
         $fname = $_POST["fname"];
         $lname = $_POST["lname"];
         $password = $_POST["password"];
-        $email = $_POST["email"];
         $ssn = $_POST["ssn"];
         $phone = $_POST["phone"];
         $lstate = $_POST["lstate"];
@@ -24,12 +23,6 @@
         $result = mysqli_query($conn, $sql);
         $info = mysqli_fetch_assoc($result);
         if ($info["status"] == "Pending") {
-            $sql= "SELECT * FROM Employees WHERE email='$email'";
-            $result = mysqli_query($conn, $sql);
-            $count = mysqli_num_rows($result);
-            if($count>0) {
-                echo "<script>alert('Registration unsuccessful.');window.location.href='employee_register.php';</script>";
-            }
             $sql= "SELECT * FROM Employees WHERE ssn='$ssn'";
             $result = mysqli_query($conn, $sql);
             $count = mysqli_num_rows($result);
@@ -48,10 +41,10 @@
             if($count>0) {
                 echo "<script>alert('Registration unsuccessful.');window.location.href='employee_register.php';</script>";
             }
-            $sql = "UPDATE Employees SET password='$password', email='$email', status='Hired', address='$address', address2='$address2', phone='$phone', state='$state', license='$license', ssn='$ssn', lstate='$lstate', city='$city', zip='$zip', fname='$fname', lname='$lname', full_license='$full_license' WHERE employee_id='$id'";
+            $sql = "UPDATE Employees SET password='$password', status='Hired', address='$address', address2='$address2', phone='$phone', state='$state', license='$license', ssn='$ssn', lstate='$lstate', city='$city', zip='$zip', fname='$fname', lname='$lname', full_license='$full_license' WHERE employee_id='$id'";
             $result = mysqli_query($conn, $sql);
             $_SESSION["new_e_token"] = FALSE;
-            header("Location: EmployeeLogin.php");
+            echo "<script>alert('Registration Successful');window.location.href='EmployeeLogin.php';</script>";
         }
         else {
             $_SESSION["new_e_token"] = FALSE;
