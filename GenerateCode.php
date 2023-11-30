15396d198eb595a8252fe7a88e8a43cbc097524f
KennethFam <147673498+KennethFam@users.noreply.github.com>
2023-11-29 19:17:17 -0800

    Update GenerateCode.php

--- GenerateCode.php
+++ GenerateCode.php
@@ -8,26 +8,28 @@
         }
         $digits = 6;
         $code =  sprintf("%06d", mt_rand(0, 999999));
-        $_SESSION['body'] = "Your multifactor authentication code is: $code.";
-        if ($_SESSION['Last_Location'] == "authentication.php") {
+        $_SESSION['body'] = "Your multifactor authentication code is: $code";
+        $_SESSION['subject'] = "Multi-Factor Authentication";
+        if ($_SESSION['Last_Location'] == "authentication.php" || $_SESSION['logged_in'] == TRUE) {
             $username = $_SESSION['username'];
             $sql = "UPDATE registrations SET TFAcode='$code' WHERE username = '$username'";
             $results = mysqli_query($conn, $sql);
         }
-        else if ($_SESSION['Last_Location'] == "employee_authentication.php") {
+        else if ($_SESSION['Last_Location'] == "employee_authentication.php" || $_SESSION['e_logged_in'] == TRUE) {
             $id = $_SESSION['id'];
             $sql = "UPDATE Employees SET TFAcode='$code' WHERE employee_id = '$id'";
             $results = mysqli_query($conn, $sql);
         }
-        else if ($_SESSION['logged_in'] == TRUE) {
-            $username = $_SESSION['username'];
-            $sql = "UPDATE registrations SET TFAcode='$code' WHERE username = '$username'";
-            $results = mysqli_query($conn, $sql);
-        }
-        else if ($_SESSION['e_logged_in'] == TRUE) {
-            $id = $_SESSION['id'];
-            $sql = "UPDATE Employees SET TFAcode='$code' WHERE employee_id = '$id'";
-            $results = mysqli_query($conn, $sql);
+        else if (($_SESSION['e_forgot'] == TRUE || $_SESSION['u_forgot'] == TRUE) && $_SESSION['Last_Location'] == "forgot_process.php") {
+            $email = $_SESSION['email'];
+            if ($_SESSION['e_forgot'] == TRUE) {
+                $sql = "UPDATE Employees SET TFAcode='$code' WHERE email = '$email'";
+                $results = mysqli_query($conn, $sql);
+            }
+            else {
+                $sql = "UPDATE registrations SET TFAcode='$code' WHERE email = '$email'";
+                $results = mysqli_query($conn, $sql);
+            }
         }
         header("Location: send-email.php");
     }
