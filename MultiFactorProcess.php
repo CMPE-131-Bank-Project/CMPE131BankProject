adaa225a46e1ad30fa9e846afa102630c3ba1b59
KennethFam <147673498+KennethFam@users.noreply.github.com>
2023-11-29 19:18:01 -0800

    Update MultiFactorProcess.php

--- MultiFactorProcess.php
+++ MultiFactorProcess.php
@@ -6,34 +6,37 @@
         if (!$conn) {
             die("Connection failed: " . mysqli_connect_error());
         }
-        if ($_SESSION['Last_Location'] == "authentication.php") {
+        if ($_SESSION['Last_Location'] == "authentication.php" || $_SESSION['logged_in'] == TRUE) {
             $username = $_SESSION['username'];
             $sql = "SELECT TFAcode FROM registrations WHERE username = '$username'";
             $results = mysqli_query($conn, $sql);
         }
-        else if ($_SESSION['Last_Location'] == "employee_authentication.php") {
+        else if ($_SESSION['Last_Location'] == "employee_authentication.php" || $_SESSION['e_logged_in'] == TRUE) {
             $id = $_SESSION['id'];
             $sql = "SELECT TFAcode FROM Employees WHERE employee_id = '$id'";
             $results = mysqli_query($conn, $sql);
         }
-        else if ($_SESSION['logged_in'] == TRUE) {
-            $username = $_SESSION['username'];
-            $sql = "SELECT TFAcode FROM registrations WHERE username = '$username'";
-            $results = mysqli_query($conn, $sql);
-        }
-        else if ($_SESSION['e_logged_in'] == TRUE) {
-            $id = $_SESSION['id'];
-            $sql = "SELECT TFAcode FROM Employees WHERE employee_id = '$id'";
-            $results = mysqli_query($conn, $sql);
+        else if ($_SESSION['e_forgot'] == TRUE || $_SESSION['u_forgot'] == TRUE) {
+            $email = $_SESSION['email'];
+            if ($_SESSION['e_forgot'] == TRUE) {
+                $sql = "SELECT TFAcode FROM Employees WHERE email = '$email'";
+                $results = mysqli_query($conn, $sql);
+            }
+            else {
+                $sql = "SELECT TFAcode FROM registrations WHERE email = '$email'";
+                $results = mysqli_query($conn, $sql);
+            }
         }
         if ($results) {
-            session_start();
             $row = mysqli_fetch_assoc($results);
             if ($row["TFAcode"] == $code) {
                 $_SESSION['TFA'] = FALSE;
                 $_SESSION['TFA_Token'] = TRUE;
-                $location = $_SESSION['Last_Location'];
-                header("Location: $location");
+                if ($_SESSION['Last_Location'] == "forgot_process.php") header("Location: reset_pass.php");
+                else {
+                    $location = $_SESSION['Last_Location'];
+                    header("Location: $location");
+                }
             } 
             else {
                 echo "<script>alert('The code that you entered was invalid.');window.location.href='MultiFactor.php';</script>";
